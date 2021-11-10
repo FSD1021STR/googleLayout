9dbf4e90327562ea73754861b18f0b39457e256c
JGamito <gamito90@hotmail.com>
2021-11-10 21:31:18 +0100

    exercise finished

--- src/googleLayout.js
+++ src/googleLayout.js
@@ -2,23 +2,27 @@ import React from "react";
 import "./googleLayout.css";
 
 export default function GoogleLayout() {
-    return(
-        <div className="container">
-            <div className="header">
-                <button>gmail</button>
-                <button>images</button>
-                <button>settings</button>
-            </div>
-            <div className="body">
-                <div className="img"/>
-                <input placeholder="Serach Google..."></input>
-                <button>Buscar</button>
-                <button>Voy a tener suerte</button>                          
-            </div>
-            <div className="footer">
-                <p>All rights reserved</p>
-                <button className="button">customize</button>
-            </div>
+  return (
+    <div className="container">
+      <div className="header">
+        <div>
+          <button>gmail</button>
+          <button>images</button>
+          <button>settings</button>
         </div>
-    )
+      </div>
+      <div className="body">
+        <div className="img" />
+        <input placeholder="Serach Google..."></input>
+        <div className="buttons">
+          <button>Buscar</button>
+          <button>Voy a tener suerte</button>
+        </div>
+      </div>
+      <div className="footer">
+        <p>All rights reserved</p>
+        <button className="footer_button">customize</button>
+      </div>
+    </div>
+  );
 }
