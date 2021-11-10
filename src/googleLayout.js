a79151493600bd1b42440a44dfd0b4fb19aef248
JMeskini <jaume.meskini@gmail.com>
2021-11-10 17:19:20 +0100

    deleted divs

--- src/googleLayout.js
+++ src/googleLayout.js
@@ -5,19 +5,15 @@ export default function GoogleLayout() {
     return(
         <div className="container">
             <div className="header">
-                <div>
-                    <button>gmail</button>
-                    <button>images</button>
-                    <button>settings</button>
-                </div>
+                <button>gmail</button>
+                <button>images</button>
+                <button>settings</button>
             </div>
             <div className="body">
                 <div className="img"/>
                 <input placeholder="Serach Google..."></input>
-                <div>
-                    <button>Buscar</button>
-                    <button>Voy a tener suerte</button>
-                </div>           
+                <button>Buscar</button>
+                <button>Voy a tener suerte</button>                          
             </div>
             <div className="footer">
                 <p>All rights reserved</p>
